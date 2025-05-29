6fd604d6b46e56f533e69012603ea5edc3a6f160
Bryan Frye <bryanfrye@gmail.com>
2025-05-29 00:37:18 -0400

    update

--- scripts/linters/lint_ansible.sh
+++ scripts/linters/lint_ansible.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+TEMPLATE_DIR="./deploy/ansible"
 command -v ansible-lint >/dev/null 2>&1 || { echo "ansible-lint not installed. Skipping."; exit 0; }
-find . -name "*.yml" -o -name "*.yaml" | grep -i "ansible" | xargs ansible-lint
+find $TEMPLATE_DIR  -name "*.playbook" | grep -i "ansible" | xargs ansible-lint
